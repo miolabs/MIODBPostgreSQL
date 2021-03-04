2e97a09c40c346041afa304fc31d6e673213fb4b
Javier Segura <javier@miolabs.com>
2021-03-04 21:16:40 +0100

    Merging

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
             name: "CLibPQ",
             pkgConfig: "libpq",
             providers: [
-                .brew(["postgresql"]),
+                .brew(["libpq"]),
                 .apt(["libpq-dev"])
             ]
         ),
