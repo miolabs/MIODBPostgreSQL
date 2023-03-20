797bb7c5e48a453e10515d08efd4ac4e33325390
Javier Segura <javier@miolabs.com>
2023-03-20 12:59:06 +0100

    mering

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MIODBPostgreSQL",
     platforms: [
-        .macOS(.v10_15), .iOS(.v11)
+        .macOS(.v10_11), .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
