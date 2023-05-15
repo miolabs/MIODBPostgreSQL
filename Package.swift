f3dfd384083ab08a3847473fb30ba9d350b79ae8
David Trallero <ditiem@gmail.com>
2023-05-15 18:58:32 +0800

    fix url => usr

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MIODBPostgreSQL",
-//    platforms: [
-//        .macOS(.v10_11), .iOS(.v14)
-//    ],
+    platforms: [
+        .macOS(.v10_15), .iOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library( name: "MIODBPostgreSQL", targets: ["MIODBPostgreSQL"]),
