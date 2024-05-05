4b0c3ba48642f2050362fa1d02b8df05e71a6485
Javier Segura <javier@miolabs.com>
2024-05-05 12:17:53 +0200

    removing memory leak

--- Sources/MIODBPostgreSQL/MIODBPostgreSQL.swift
+++ Sources/MIODBPostgreSQL/MIODBPostgreSQL.swift
@@ -24,14 +24,17 @@ extension MIODBPostgreSQLError: LocalizedError {
     }
 }
 
-open class MIODBPostgreSQL: MIODB {
-
+open class MIODBPostgreSQL: MIODB 
+{
     let defaultPort:Int32 = 5432
     let defaultUser = "root"
     let defaultDatabase = "public"
     
-    var connection:OpaquePointer?
+    var _connection:OpaquePointer?
+    var _connection_str: [CChar]?
 
+    deinit { disconnect() }
+    
     open override func connect( _ to_db: String? = nil ) throws {
         if port == nil { port = defaultPort }
         if user == nil { user = defaultUser }
@@ -41,13 +44,16 @@ open class MIODBPostgreSQL: MIODB {
         
         //let connectionString = "host = \(host!) port = \(port!) user = \(user!) password = \(password!) dbname = \(database!) gssencmode='disable'"
         connectionString = "host = \(host!) port = \(port!) user = \(user!) password = \(password!) dbname = \(final_db)"
-        connection = PQconnectdb(connectionString!.cString(using: .utf8))
-
-        let status = PQstatus(connection)
+        _connection_str = connectionString!.cString(using: .utf8)
+        
+        _connection = PQconnectdb( _connection_str )
+        let status = PQstatus( _connection )
         if  status != CONNECTION_OK {
-            connection = nil
+            _connection = nil
             throw MIODBPostgreSQLError.fatalError("-1", "Could not connect to POSTGRESQL Database. Connection string: \(connectionString!)")
         }
+        
+        try super.connect(to_db)
     }
     
 //    open func connect(scheme:String?) throws {
@@ -56,8 +62,11 @@ open class MIODBPostgreSQL: MIODB {
 //    }
     
     open override func disconnect() {
-        PQfinish(connection)
-        connection = nil
+        if _connection != nil {
+            PQfinish( _connection )
+            _connection = nil
+            _connection_str = nil
+        }
     }
     
     @discardableResult open override func executeQueryString(_ query:String) throws -> [[String : Any]]? {
@@ -76,148 +85,124 @@ open class MIODBPostgreSQL: MIODB {
     }
     
     @discardableResult open func _executeQueryString(_ query:String) throws -> [[String : Any]]? {
-        
-//        if isInsideTransaction {
-//            pushQueryString(query)
-//            return []
-//        }
-        
-        if ( PQstatus(connection) != CONNECTION_OK ) {
+                
+        if ( PQstatus( _connection ) != CONNECTION_OK ) {
             print( "[[FATAL ERROR]: Postgres connection was lost, re-connecting and crossing fingers")
             disconnect()
             usleep( 500000 ) // 0.5 seconds
             try connect()
         }
-
-
-        let res = PQexec(connection, Array(query.utf8CString))
         
-        defer {
-            PQclear(res)
-        }
-        
-        var items:[[String : Any]] = []
-        
-        switch PQresultStatus(res) {
+        let r = try query.withCString { query_cstr in
+            let res = PQexec( _connection, query_cstr )
+            
+            defer { PQclear(res) }
+            
+            var items:[[String : Any]] = []
+            
+            switch PQresultStatus(res) {
+                
+            case PGRES_COMMAND_OK: break
+                
+            case PGRES_TUPLES_OK:
+                for row in 0..<PQntuples(res) {
+                    var item: [String:Any] = [:]
+                    for col in 0..<PQnfields(res){
+                        let colname = String(cString: PQfname(res, col))
                         
-        case PGRES_EMPTY_QUERY:
-            print("Empty query")
-            
-        case PGRES_COMMAND_OK:
-            break
-//            print("Command OK")
-            
-        case PGRES_TUPLES_OK:
-            for row in 0..<PQntuples(res) {
-                var item: [String:Any] = [:]
-                for col in 0..<PQnfields(res){
-                    let colname = String(cString: PQfname(res, col))
-                    
-                    if PQgetisnull(res, row, col) == 1 {
-                        item[ colname ] = NSNull( )
-                        continue
+                        if PQgetisnull(res, row, col) == 1 {
+                            item[ colname ] = NSNull( )
+                            continue
+                        }
+                        
+                        let type = PQftype(res, col)
+                        let value = PQgetvalue(res, row, col)
+                        
+                        item[colname] = try convert(value: value!, withType: type)
                     }
-                    
-                    let type = PQftype(res, col)
-                    let value = PQgetvalue(res, row, col)
-                    
-                    item[colname] = try convert(value: value!, withType: type)
+                    items.append(item)
                 }
-                items.append(item)
+                
+            case PGRES_FATAL_ERROR:
+                let errorMessage = (scheme != nil ? "\(scheme!): " : "") + String(cString: PQresultErrorMessage(res)) + "\n" + query
+                let err_code = PQresultErrorField(res, 67 )
+                let code = err_code != nil ? String( cString: err_code! ) : "0"
+                throw MIODBPostgreSQLError.fatalError(code, errorMessage)
+                
+            case PGRES_EMPTY_QUERY    : print("Empty query")
+            case PGRES_COPY_OUT       : print("Copy out")
+            case PGRES_COPY_IN        : print("Copy in")
+            case PGRES_BAD_RESPONSE   : print("Bad response")
+            case PGRES_NONFATAL_ERROR : print("Non fatal error")
+            case PGRES_COPY_BOTH      : print("Copy both")
+            case PGRES_SINGLE_TUPLE   : print("Single tupple")
+                
+            default: print("Response not implemented.")
             }
             
-        case PGRES_COPY_OUT:
-            print("Copy out")
-            
-        case PGRES_COPY_IN:
-            print("Copy in")
-            
-        case PGRES_BAD_RESPONSE:
-            print("Bad response")	
-            
-        case PGRES_NONFATAL_ERROR:
-            print("Non fatal error")
-              
-        case PGRES_FATAL_ERROR:
-            let errorMessage = (scheme != nil ? "\(scheme!): " : "") + String(cString: PQresultErrorMessage(res)) + "\n" + query
-            let err_code = PQresultErrorField(res, 67 )
-            let code = err_code != nil ? String( cString: err_code! ) : "0"
-            throw MIODBPostgreSQLError.fatalError(code, errorMessage)
-            
-        case PGRES_COPY_BOTH:
-            print("Copy both")
-            
-        case PGRES_SINGLE_TUPLE:
-            print("Single tupple")
-        
-        default:
-            print("Response not implemented." +  String(cString: PQresultErrorMessage(res)))
-        
+            return items
         }
-                            
-        return items
+        
+        return r
     }
     
     func convert ( value:UnsafePointer<Int8>, withType type:Oid ) throws -> Any {
         if type == 16 { // Boolean
             return (value[0] == 116) as Bool
         }
-
         
         var ret:Any?
+        let str = String(cString: value)
 
+        switch type {
 
-        try MIOCoreAutoReleasePool {
-            let str = String(cString: value)
+        case 20: ret = MIOCoreInt64Value( str )! // Int8
+        case 23: ret = MIOCoreInt32Value( str )! // Int4
+        case 21: ret = MIOCoreInt16Value( str )! // Int2
+        case 18: ret = MIOCoreInt8Value(  str )! // UInt8, char
+            
+        case 1700, 700, 701, 790: // numeric, float4, float8, money
+            ret = Decimal( string: str )!
+            
+        case 1114: ret = MIOCoreDate( fromString: str ) // Timestamp
+        case 1184: ret = MIOCoreDate( fromString: str ) // Timestamp Z
+        case 1083: ret = str // TODO: Time
 
-            switch type {
+        case 1043: // varchar
+            ret = str
+        case 114, 3802: // json, jsonb (= transformable for us)
+            ret = try JSONSerialization.jsonObject(with: str.data(using: .utf8)!, options: [.allowFragments] )
+        case 3807: // json binary array
+            ret = try JSONSerialization.jsonObject(with: str.data(using: .utf8)!, options: [.allowFragments] )
 
-            case 20: ret = MIOCoreInt64Value( str )! // Int8
-            case 23: ret = MIOCoreInt32Value( str )! // Int4
-            case 21: ret = MIOCoreInt16Value( str )! // Int2
-            case 18: ret = MIOCoreInt8Value(  str )! // UInt8, char
-                
-            case 1700, 700, 701, 790: // numeric, float4, float8, money
-                ret = Decimal( string: str )!
-                
-            case 1114: ret = MIOCoreDate( fromString: str ) // Timestamp
-            case 1184: ret = MIOCoreDate( fromString: str ) // Timestamp Z
-            case 1083: ret = str // TODO: Time
-
-            case 1043: // varchar
-                ret = str
-            case 114, 3802: // json, jsonb (= transformable for us)
-                ret = try JSONSerialization.jsonObject(with: str.data(using: .utf8)!, options: [.allowFragments] )
-            case 3807: // json binary array
-                ret = try JSONSerialization.jsonObject(with: str.data(using: .utf8)!, options: [.allowFragments] )
-
-            case 2950: // UUID
-                ret = UUID( uuidString: str )
-                
-            case 25,19: // Text, Name(used when getting information from the DB as which contraints/indices/etc has)
-                ret = str
-                
-            case 1082: // date
-                ret = MIOCoreDate( fromString: str )
-                
-            default:
-                NSLog("Type not implemented. Fallback to string. type: \(type)")
-                ret = str
-            }
+        case 2950: // UUID
+            ret = UUID( uuidString: str )
+            
+        case 25,19: // Text, Name(used when getting information from the DB as which contraints/indices/etc has)
+            ret = str
+            
+        case 1082: // date
+            ret = MIOCoreDate( fromString: str )
+            
+        case 2278: // void
+            ret = str
+        default:
+            NSLog("Type not implemented. Fallback to string. type: \(type)")
+            ret = str
         }
         
         return ret!
     }
     
     open override func changeScheme(_ scheme:String?) throws {
-        if connection == nil {
+        if scheme == nil { return }
+        
+        if _connection == nil {
             throw MIODBPostgreSQLError.fatalError("-2","Could not change the scheme. The connection is nil")
         }
-        
-        if scheme != nil {
-            try executeQueryString("SET search_path TO \(scheme!), public")
-            self.scheme = scheme!
-        }
+                
+        try executeQueryString("SET search_path TO \(scheme!), public")
+        try super.changeScheme(scheme)
     }
     
 }
--- Tests/MIODBPostgreSQLTests/MIODBPostgreSQLTests.swift
+++ Tests/MIODBPostgreSQLTests/MIODBPostgreSQLTests.swift
@@ -2,14 +2,33 @@ import XCTest
 @testable import MIODBPostgreSQL
 
 final class MIODBPostgreSQLTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        //XCTAssertEqual(MIODBPostgreSQL().text, "Hello, World!")
+    func testConnection() {
+        let host_key = ProcessInfo.processInfo.environment["HOST"] ?? ""
+        let user_key = ProcessInfo.processInfo.environment["USER"] ?? ""
+        let pass_key = ProcessInfo.processInfo.environment["PASS"] ?? ""
+        let db_key   = ProcessInfo.processInfo.environment["DB"] ?? ""
+        let sch_key   = ProcessInfo.processInfo.environment["SCHEMA"] ?? ""
+        
+        let db = MIODBPostgreSQL(host: host_key, user: user_key, password: pass_key, database: db_key, scheme: "_" + sch_key.replacing("-", with: "") )
+        var found_key = false
+        do {
+            if let rows = try db.executeQueryString( "select * from server_status" ) {
+                for r in rows {
+                    if let key = r["key"] as? String {
+                        if key == "sync_files" { found_key = true }
+                        print( "# \( key )\t\( r["server_id"]! )\t\( r["value"]! )\t\( r["processing"]! )")
+                    }
+                }
+            }
+        }
+        catch {
+            print( "\(error.localizedDescription )" )
+        }
+        
+        XCTAssert( found_key )
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testConnection", testConnection),
     ]
 }
