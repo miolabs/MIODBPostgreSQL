0448929d1700efce926cdc015f64683199b101f9
Javier Segura <javier@miolabs.com>
2025-05-28 12:16:16 +0200

    adding conenction protocol delegate

--- Sources/MIODBPostgreSQL/MIODBPostgreConnection.swift
+++ Sources/MIODBPostgreSQL/MIODBPostgreConnection.swift
@@ -1,6 +1,6 @@
 //
-//  File.swift
-//  
+//  MIODBPostgreConnection.swift
+//  MIODBPostgreSQL
 //
 //  Created by David Trallero on 26/11/2020.
 //
@@ -11,8 +11,9 @@ import MIODB
 
 open class MDBPostgreConnection : MDBConnection
 {
-    open override func create ( _ to_db: String?, identifier: String? = nil, label: String? = nil ) throws -> MIODB {
+    open override func create ( _ to_db: String?, identifier: String? = nil, label: String? = nil, delegate: MDBDelegate? = nil ) throws -> MIODB {
         let db = MIODBPostgreSQL( connection: self )
+        db.delegate = delegate
         if let id = identifier { db.identifier = id }
         if let lbl = label { db.label = lbl }
         try db.connect( to_db )
